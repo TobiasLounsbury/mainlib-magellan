9e1eb7c561c516e78f5b14bcd7561c5a6d8c3b67
Tobias Lounsbury <tobiaslounsbury@gmail.com>
2019-11-13 11:51:03 -0800

    Fixed regressions in the library service listing page and refactored it to use the new default icon setting rather than the hard-coded class

--- templates/list-library-services.php
+++ templates/list-library-services.php
@@ -3,10 +3,12 @@
 //Get terms for wpsl_store_category
 $terms = get_terms( array(
     'taxonomy' => 'wpsl_store_category',
+    'orderby' => "term_order",
     'hide_empty' => false,
 ) );
 $totalLocations = (int) wp_count_posts("wpsl_stores")->publish;
 $locationsPath = get_option("mainlib_magellan_locations_path");
+$defaultIcon = get_option("mainlib_magellan_default_icon");
 foreach($terms as $term) {
 include(__DIR__."/parts/list-single-service.php");
 }
--- templates/parts/list-single-service.php
+++ templates/parts/list-single-service.php
@@ -1,13 +1,15 @@
 <?php
 $customFields = get_fields("term_{$term->term_id}");
-$icon = ($customFields && $customFields['service_icon']) ? $customFields['service_icon'] : "<i class='fas fa-concierge-bell' aria-hidden='true'></i>";
+$iconClass = ($customFields && array_key_exists("service_icon", $customFields)) ? $customFields['service_icon'] : $defaultIcon;
 ?>
 <div class="library-service-listing">
-  <div class="library-service-title"><?php echo $term->name; ?></div>
-  <div class="library-service-details">
-      <div class="library-service-icon"><i class="<?php echo $icon; ?>"></i></div>
-    <div class="library-service-description"><?php echo $term->description; ?></div>
-    <div class="library-service-permalink"><a href="/<?php echo $locationsPath; ?>/?wpsl-widget-categories=<?php echo $term->term_id; ?>#wpsl-result-list">Sites with this Service</a></div>
-  </div>
-  <div class="library-service-usage"><?php echo round(($term->count / $totalLocations) * 100); ?>% of MAIN Libraries offer <?php echo $term->name; ?>.</div>
+    <div class="library-service-title"><strong><?php echo $term->name; ?></strong></div>
+    <div class="library-service-details">
+        <div class="library-service-icon"><i class="<?php echo $iconClass; ?>"></i></div>
+        <div class="mainlib-no-after">
+            <div class="library-service-description"><?php echo $term->description; ?></div>
+            <div class="library-service-permalink"><a href="/<?php echo $locationsPath; ?>/?wpsl-widget-categories=<?php echo $term->term_id; ?>#wpsl-result-list">Sites with this Service</a></div>
+            <div class="library-service-usage"><?php echo round(($term->count / $totalLocations) * 100); ?>% of MAIN Libraries offer <?php echo $term->name; ?>.</div>
+        </div>
+    </div>
 </div>
