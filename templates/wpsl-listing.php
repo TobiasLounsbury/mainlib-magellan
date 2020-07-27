8a255481b7455c641ec2c8c621eb54659023f213
Tobias Lounsbury <tobiaslounsbury@gmail.com>
2020-07-27 16:01:52 -0700

    Changed the scaling logic around a little bit so that it more accurately reflects the scales that seem to work best when adjust by hand, as well as hading a changing in the viewBox Height param to get smaller text more vertically centered

--- templates/wpsl-listing.php
+++ templates/wpsl-listing.php
@@ -36,6 +36,14 @@ $defaultIcon = get_option('mainlib_magellan_default_icon');
 
               //$displayName = (strlen($term->name) > 18) ? substr($term->name, 0, 15) . "..." : $term->name;
               $displayName = $term->name;
+                $boxWidth = 80;
+                $boxHeight = 20;
+                if (strlen($displayName) > 16) {
+                    $diff = strlen($displayName) - 16;
+                    $boxWidth = round( $diff * 4.25) + 80;
+                    $boxHeight = 20 + $diff;
+                }
+
               ?>
               <figure class='library-listing-single-service <% if ( _.contains(services, '<?php echo $term->slug; ?>') ) { %>service-available<% } %>'>
               <div class='library-service-icon'><svg xmlns="http://www.w3.org/2000/svg" version="1.1" viewBox="0 0 56 56" class="library-service-icon-svg">
@@ -43,7 +51,7 @@ $defaultIcon = get_option('mainlib_magellan_default_icon');
                       <circle class="library-service-icon-svg-available-background" cx="45" cy="44" r="7"/>
                       <text x="45" y="45" class="library-service-icon-svg-available" >&#xf058;</text>
                   </svg></div>
-              <figcaption class='library-service-icon-title'><svg xmlns="http://www.w3.org/2000/svg" version="1.1" viewBox="0 0 <?php echo (strlen($displayName) > 16) ? ((strlen($displayName) - 16) * 6) + 80 : 80; ?> 20" class="library-service-icon-title-svg">
+              <figcaption class='library-service-icon-title'><svg xmlns="http://www.w3.org/2000/svg" version="1.1" viewBox="0 0 <?php echo $boxWidth; ?> <?php echo $boxHeight; ?>" class="library-service-icon-title-svg">
                       <text x="50%" y="55%" dominant-baseline="middle" text-anchor="middle" class="library-service-icon-title-svg-text" ><?php echo $displayName; ?></text>
                   </svg></figcaption></figure><?php
             if(!array_key_exists("default_service", $meta) || !$meta['default_service']) {
